118d24daca40426071aa90d429578c10edd50f2f
sawantxsumit <sawantsumit297@gmail.com>
2026-01-31 20:41:03 +0530

    strucutred summary

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from text_summarization import preprocess_text , score_sentences , calculate_tfidf , generate_summary
 
 import streamlit as st
-
+from nltk import sent_tokenize
 st.title("CONCISE")
 
 text= st.text_area("Enter the text you want to summarize ")
@@ -9,16 +9,23 @@ text= st.text_area("Enter the text you want to summarize ")
 if st.button("Submit"):
     try:
         st.write("Preprocessing....")
-        sentences= preprocess_text(text)
+        sentence= preprocess_text(text)
         st.write("Calculating TF_IDF scores....")
-        tf_idf= calculate_tfidf(sentences)
+        tf_idf= calculate_tfidf(sentence)
         st.write("Scoring sentences....")
         sentence_score= score_sentences(tf_idf)
-        summary= generate_summary(sentences , sentence_score)
-        st.subheader('Original text :')
+        summary= generate_summary(sentence , sentence_score)
+        st.subheader('📌 Original text :')
         st.markdown(text)
-        st.subheader('Generated summary :')
-        st.success(summary)
+        summary_text = summary
+
+        sentences = sent_tokenize(summary_text)
+
+        st.subheader("📌 Generated Summary")
+
+        for sent in sentences:
+            st.markdown(f"-> {sent}")
+        # st.success(summary)
     except:
         st.error("Some error occured ")
 
